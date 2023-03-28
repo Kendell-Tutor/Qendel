8cdfa55be23e132c10da4d1d423fc6486deedc51
lily <128761243+Lilyt15@users.noreply.github.com>
2023-03-28 19:21:47 -0400

    tutoring repo and service created

--- Tutoring Service/src/main/java/com/kendeltutoring/model/Tutoring.java
+++ Tutoring Service/src/main/java/com/kendeltutoring/model/Tutoring.java
@@ -1,12 +1,24 @@
 package com.kendeltutoring.model;
 
-public class Tutoring {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tutoring {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
     private String experience;
     private String Course;
+    @Embedded
     private Address address;
 
 }
